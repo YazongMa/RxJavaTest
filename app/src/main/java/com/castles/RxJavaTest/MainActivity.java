79c80bd5a3902a5a44bb8aa78c93a31e49c7a38a
YazongMa <mayazong@126.com>
2023-02-26 21:19:48 +0800

    Initial commit

---
+++ app/src/main/java/com/castles/RxJavaTest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.castles.RxJavaTest;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final String PATH = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA17qBbp.img";
+
+    Button button = findViewById(R.id.test);
+    ImageView imageView = findViewById(R.id.test_image);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: " + this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadImageRxJava();
+            }
+        });
+    }
+
+    private void downloadImageRxJava(){
+    }
+}
