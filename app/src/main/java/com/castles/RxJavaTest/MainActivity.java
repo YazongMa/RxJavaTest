f76d760a69947c479ba1272843bca97212c78196
YazongMa <mayazong@126.com>
2023-02-27 13:42:59 +0800

    Initial commit

--- app/src/main/java/com/castles/RxJavaTest/MainActivity.java
+++ app/src/main/java/com/castles/RxJavaTest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.castles.RxJavaTest;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +10,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
@@ -22,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final String PATH = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA17qBbp.img";
 
-    Button button = findViewById(R.id.test);
-    ImageView imageView = findViewById(R.id.test_image);
+    private Button button;
+    private ImageView imageView;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +39,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: " + this);
+        button = findViewById(R.id.test);
+        imageView = findViewById(R.id.test_image);
+        progressDialog = new ProgressDialog(this);
+
         button.setOnClickListener(view -> downloadImageRxJava());
     }
 
     private void downloadImageRxJava(){
         Observable.just(PATH)
-                .map((Function<String, Bitmap>) s -> {
+                .map(s -> {
+                    URL url = new URL(PATH);
+                    HttpsURLConnection httpsURLConnection = (HttpsURLConnection)url.openConnection();
+                    httpsURLConnection.setConnectTimeout(5000);
+                    int responseCode = httpsURLConnection.getResponseCode();
+                    if (responseCode == HttpsURLConnection.HTTP_OK) {
+                        InputStream is = httpsURLConnection.getInputStream();
+                        return BitmapFactory.decodeStream(is);
+                    }
                     return null;
                 })
                 .subscribeOn(Schedulers.io())
@@ -44,22 +64,28 @@ public class MainActivity extends AppCompatActivity {
                 .subscribe(new Observer<Bitmap>() {
                     @Override
                     public void onSubscribe(Disposable d) {
-
+                        progressDialog.setView(imageView);
+                        progressDialog.setTitle("test");
+                        progressDialog.setMessage("in progress");
+                        progressDialog.show();
+                        Log.d(TAG, "onSubscribe: " + this);
                     }
 
                     @Override
                     public void onNext(Bitmap bitmap) {
-
+                        imageView.setImageBitmap(bitmap);
+                        Log.d(TAG, "onNext: " + this);
                     }
 
                     @Override
                     public void onError(Throwable e) {
-
+                        Log.d(TAG, "onError: " + this);
                     }
 
                     @Override
                     public void onComplete() {
-
+                        progressDialog.dismiss();
+                        Log.d(TAG, "onComplete: " + this);
                     }
                 });
     }
