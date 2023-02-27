b4d65ea0234d04ac757911b7e9e5a104852cbf9e
YazongMa <mayazong@126.com>
2023-02-27 12:04:21 +0800

    Initial commit

--- app/src/main/java/com/castles/RxJavaTest/MainActivity.java
+++ app/src/main/java/com/castles/RxJavaTest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.castles.RxJavaTest;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +10,12 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void downloadImageRxJava(){
+        Observable.just(PATH)
+                .map((Function<String, Bitmap>) s -> {
+                    return null;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Bitmap>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Bitmap bitmap) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
